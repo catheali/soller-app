7e7a18fe27df4ed1f45d01a7107a3a49a33f6413
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-11-03 22:18:08 -0300

    feat: add new  template components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import './assets/fonts/fonts.css'
+import {Header} from './templates/Header/Header';
+import {Footer} from './templates/Footer/Footer';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header/>
+	<Footer/>
   </StrictMode>,
 )
