146567842274da4a642cd6483df557fa89d5469d
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-11-23 14:50:46 -0300

    feat: add new component ButtonQuote

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import './assets/fonts/fonts.css'
 import {Header} from './templates/Header/Header';
 import {Footer} from './templates/Footer/Footer';
+import {ButtonQuote} from './components/ButtonQuote/ButtonQuote';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header/>
+	<ButtonQuote color='#581C87'/>
 	<Footer/>
   </StrictMode>,
 )
